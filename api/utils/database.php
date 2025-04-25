50f50fddfdbefb008deffbfeb49691a2fcd34b95
Tomss <116651226+Thomas77500@users.noreply.github.com>
2025-04-25 12:07:25 +0200

    Update database.php

--- api/utils/database.php
+++ api/utils/database.php
@@ -1,28 +1,17 @@
 <?php
-<<<<<<< HEAD
+
 require_once $_SERVER['DOCUMENT_ROOT'] . '/api/utils/server.php';
 
 function getDatabaseConnection(): PDO {
     try {
         $host = 'localhost';
-        $db = 'vikings';
+        $db = 'viking';
         $user = 'root';
-        $pass = 'root';
-        $port = '8889';
+        $pass = '';
+        $port = '3306';
         return new PDO("mysql:host=$host;dbname=$db;port=$port", $user, $pass);
     } catch (PDOException $e) {
         returnError(500, 'Could not connect to the database. ' . $e->getMessage());
         die();
     }
-=======
 
-function getDatabaseConnection(): PDO
-{
-    $dbname = 'vikings';
-    $port = 8889;
-    $user = 'root';
-    $pwd = 'root';
-    $host = 'localhost';
-    return new PDO("mysql:host=$host;dbname=$dbname;charset=utf8;port=$port", $user, $pwd);
->>>>>>> de224af (Initial commit : API Vikings avec Weapon et Viking endpoints)
-}
